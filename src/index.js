364db277a077f5e379801eedaba3a0af4d3a0414
Dinis Pereira <dinisazpereira@gmail.com>
2025-03-31 21:23:07 +0100

    Logic to add task and project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,76 @@
-alert("esta feito menino ")
+import { Task, Project } from './task.js'; // Importa a classe Task e Project
+
+const taskModal = document.getElementById('taskModal');
+const addTaskButton = document.querySelector(".addTaskButton")
+const submitButton = document.querySelector(".submitButton");
+const addProjectButton = document.querySelector(".addProject");
+const projectModal = document.getElementById("projectModal");
+const submitButton2 = document.querySelector(".submitButton2");
+
+// Função para abrir o modal
+
+function openTaskModal() {
+  taskModal.showModal();
+}
+
+// Função para fechar o modal
+function closeTaskModal() {
+  taskModal.close();
+}
+
+function openProjectModal() {
+  projectModal.showModal();
+}
+
+// Função para fechar o modal de projeto
+function closeProjectModal() {
+  projectModal.close();
+}
+
+// Adiciona evento de clique ao botão "Adicionar Projeto"
+addProjectButton.addEventListener("click", () => {
+  openProjectModal();
+});
+
+addTaskButton.addEventListener("click", () => {
+    openTaskModal();
+  });
+
+  function addProject () {
+    const projectNameInput = document.getElementById("projectName").value;
+
+    const newProject = new Project(projectNameInput)
+
+    console.log("Aqui esta o teu projeto fresquinho: ", newProject);
+
+  }
+
+  
+  function addTask () {
+    const taskTitle = document.getElementById("taskTitle").value;
+    const taskDescription = document.getElementById("taskDescription").value;
+    const taskDate = document.getElementById("taskDate").value;
+    const priorityOption = document.getElementById("priorityOption").value;
+
+    const newTask = new Task(taskTitle, taskDescription, taskDate, priorityOption); 
+
+    console.log(newTask);
+
+
+
+}
+
+submitButton.addEventListener("click", () => {
+
+  addTask();
+})
+
+submitButton2.addEventListener("click", () => {
+
+  addProject();
+})
+
+
+
+
+  
---
+++ src/task.js
@@ -0,0 +1,35 @@
+// task.js
+export class Task {
+    constructor(title, description, duedate, priority) {
+        this.title = title;
+        this.description = description;
+        this.duedate = duedate;
+        this.priority = priority;
+        this.createdAt = new Date(); // Data de criação
+    }
+
+    complete() {
+        this.completed = true;
+    }
+
+    uncomplete() {
+        this.completed = false;
+    }
+
+    getDetails() {
+        return `${this.description} (${this.completed ? 'Concluída' : 'Pendente'}) - Criada em: ${this.createdAt.toLocaleString()}`;
+    }
+}
+
+export class Project {
+    constructor(title,) {
+        this.title = title;
+       
+    }
+
+    getProject() {
+        return `${this.title}`;
+    }
+
+    
+}
